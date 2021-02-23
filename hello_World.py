e91aafaf3f7e57eccfb59326288a5964f27cb964
shaik jeelan basha <jeelanbashaismgk786@gmail.com>
2021-02-23 13:18:31 +0530

    first file

---
+++ hello_World.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+
+#chromedriver="C:\\Users\\MASTHAN\\eclipse-workspace\\Maven_selenium\\Resources\\chromedriver.exe"
+driver= webdriver.Chrome(r"C:\Users\MASTHAN\eclipse-workspace\Maven_selenium\Resources\chromedriver.exe")
+driver.get('https://www.fb.com/')
